a9dd672c1ad072c21feb41cd44f68c03afde6ff7
unknown <monika.raj3@gmail.com>
2020-05-20 09:17:58 +0530

    new file push

---
+++ src/JavaConcepts/TestADD.java
@@ -0,0 +1,23 @@
+package JavaConcepts;
+
+public class TestADD {
+	
+	int a,b,c;
+	
+	public void add()
+	{
+		a = 100;
+		b = 1000;
+		c = a + b;
+		System.out.println("Sum is: "+c);
+	}
+
+	public static void main(String[] args) {
+
+		TestADD obj = new TestADD();
+		
+		obj.add();
+
+	}
+
+}
